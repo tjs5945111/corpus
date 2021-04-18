8b4fac17bdf538384f916729ee729b52ce6650d8
童建设 <you@example.com>
2021-04-18 19:17:05 +0800

    feat:搜索

--- src/components/homeComponents/RightContent/Pc/SystemCompontent.js
+++ src/components/homeComponents/RightContent/Pc/SystemCompontent.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
 import { connect } from 'dva';
-import { Form, Icon, Input, Button } from 'antd';
+import { Form, Icon, Input, Button, Divider, Collapse, Card } from 'antd';
 import { Link } from 'react-router-dom';
-import styles from './system.css';
+import styles from './system.less';
+
+const { Panel } = Collapse;
 
 const SystemCompontent = ({ dispatch, form: { getFieldDecorator, validateFields } }) => {
   const [type, setType] = useState('login');
+  function callback(key) {
+    console.log(key);
+  }
   function handleSubmit(e) {
     e.preventDefault()
     validateFields((err, values) => {
@@ -17,29 +22,183 @@ const SystemCompontent = ({ dispatch, form: { getFieldDecorator, validateFields 
   }
   return (
     <div className={styles.system}>
-      <Form onSubmit={handleSubmit} className="login-form">
-        <Form.Item>
-          {getFieldDecorator('username', {
-            rules: [{ required: true, message: '请输入!' }],
-          })(
-            <Input
-              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              placeholder="请输入"
-              size="small"
-            />,
-          )}
-        </Form.Item>
-        <Form.Item>
-          {getFieldDecorator('password', {
-            rules: [{ required: true, message: '请输入!' }],
-          })(
-            <Input
-              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              type="password"
-              placeholder="请输入"
-            />,
-          )}
-        </Form.Item>
+      <Form onSubmit={handleSubmit} className="login-form" layout='inline'>
+        <Card style={{ marginBottom: "20px" }}>
+          <h3>高级检索</h3>
+          <Divider />
+          <Form.Item label='国家地区'>
+            {getFieldDecorator('username', {
+              rules: [{ required: true, message: '请输入!' }],
+            })(
+              <Input
+                placeholder="请输入"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item label='国家地区'>
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: '请输入!' }],
+            })(
+              <Input
+                type="password"
+                placeholder="请输入"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item label='国家地区'>
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: '请输入!' }],
+            })(
+              <Input
+                type="password"
+                placeholder="请输入"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item label='国家地区'>
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: '请输入!' }],
+            })(
+              <Input
+                type="password"
+                placeholder="请输入"
+              />,
+            )}
+          </Form.Item>
+        </Card>
+
+        <Card style={{ marginBottom: "20px" }}>
+          <h3>高级检索</h3>
+          <Divider />
+          <Form.Item label='国家地区'>
+            {getFieldDecorator('username', {
+              rules: [{ required: true, message: '请输入!' }],
+            })(
+              <Input
+                placeholder="请输入"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item label='国家地区'>
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: '请输入!' }],
+            })(
+              <Input
+                type="password"
+                placeholder="请输入"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item label='国家地区'>
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: '请输入!' }],
+            })(
+              <Input
+                type="password"
+                placeholder="请输入"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item label='国家地区'>
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: '请输入!' }],
+            })(
+              <Input
+                type="password"
+                placeholder="请输入"
+              />,
+            )}
+          </Form.Item>
+        </Card>
+
+        <Card style={{ marginBottom: "20px" }}>
+          <h3>高级检索</h3>
+          <Divider />
+          <Form.Item label='国家地区'>
+            {getFieldDecorator('username', {
+              rules: [{ required: true, message: '请输入!' }],
+            })(
+              <Input
+                placeholder="请输入"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item label='国家地区'>
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: '请输入!' }],
+            })(
+              <Input
+                type="password"
+                placeholder="请输入"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item label='国家地区'>
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: '请输入!' }],
+            })(
+              <Input
+                type="password"
+                placeholder="请输入"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item label='国家地区'>
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: '请输入!' }],
+            })(
+              <Input
+                type="password"
+                placeholder="请输入"
+              />,
+            )}
+          </Form.Item>
+        </Card>
+
+        <Card style={{ marginBottom: "20px" }}>
+          <h3>高级检索</h3>
+          <Divider />
+          <Form.Item label='国家地区'>
+            {getFieldDecorator('username', {
+              rules: [{ required: true, message: '请输入!' }],
+            })(
+              <Input
+                placeholder="请输入"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item label='国家地区'>
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: '请输入!' }],
+            })(
+              <Input
+                type="password"
+                placeholder="请输入"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item label='国家地区'>
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: '请输入!' }],
+            })(
+              <Input
+                type="password"
+                placeholder="请输入"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item label='国家地区'>
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: '请输入!' }],
+            })(
+              <Input
+                type="password"
+                placeholder="请输入"
+              />,
+            )}
+          </Form.Item>
+        </Card>
+
         <Form.Item>
           <Button type="primary" htmlType="submit" className={styles.loginformbutton}>
             重置
--- src/components/homeComponents/RightContent/RightContent.js
+++ src/components/homeComponents/RightContent/RightContent.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Layout} from 'antd';
-import { Route ,Switch} from 'react-router-dom';
+import { Layout } from 'antd';
+import { Route, Switch } from 'react-router-dom';
 import SystemCompontent from '../RightContent/Pc/SystemCompontent'
 import ServiceCompontent from '../RightContent/Pc/ServiceCompontent'
 import TopicCompontent from '../RightContent/Pc/TopicCompontent'
@@ -12,25 +12,25 @@ import QueryCompontent from '../RightContent/Pc/QueryCompontent'
 import StatisticsCompontent from '../RightContent/Pc/StatisticsCompontent'
 import FuncDicCompontent from '../RightContent/Pc/FuncDicCompontent'
 
-const { Content} = Layout;
+const { Content } = Layout;
 const RightContent = (props) => {
- 
+
   return (
-    <Content style={{margin: '24px 16px', padding: 15, background: '#fff'}}>
-         <Switch>
-            <Route path={`/pc/source/system`} component={SystemCompontent} />
-            <Route path={`/pc/source/service`} component={ServiceCompontent} />
-            <Route path={`/pc/source/topic`} component={TopicCompontent} />
-            <Route path={`/pc/source/dataContent`} component={DataContentCompontent} />
-            <Route path={`/pc/usermanage/organization`} component={OrganizationCompontent} />
-            <Route path={`/pc/usermanage/users`} component={UsersCompontent} />
-            <Route path={`/pc/logs/system`} component={LogsSystemCompontent} />
-            <Route path={`/pc/logs/query`} component={QueryCompontent} />
-            <Route path={`/pc/logs/statistics`} component={StatisticsCompontent} />
-            <Route path={`/pc/dataDic/funcDic`} component={FuncDicCompontent} />
-          </Switch>
+    <Content style={{ margin: '24px 16px', padding: 15, overflowY: "scroll", height: '38rem' }} >
+      <Switch>
+        <Route path={`/pc/source/system`} component={SystemCompontent} />
+        <Route path={`/pc/source/service`} component={ServiceCompontent} />
+        <Route path={`/pc/source/topic`} component={TopicCompontent} />
+        <Route path={`/pc/source/dataContent`} component={DataContentCompontent} />
+        <Route path={`/pc/usermanage/organization`} component={OrganizationCompontent} />
+        <Route path={`/pc/usermanage/users`} component={UsersCompontent} />
+        <Route path={`/pc/logs/system`} component={LogsSystemCompontent} />
+        <Route path={`/pc/logs/query`} component={QueryCompontent} />
+        <Route path={`/pc/logs/statistics`} component={StatisticsCompontent} />
+        <Route path={`/pc/dataDic/funcDic`} component={FuncDicCompontent} />
+      </Switch>
     </Content>
-    
+
   );
 
 };
