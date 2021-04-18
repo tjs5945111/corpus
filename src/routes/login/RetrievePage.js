74b045d98ac740450857a062da2425cd72296a6b
童建设 <you@example.com>
2021-04-18 14:39:55 +0800

    修改重置密码

--- src/routes/login/RetrievePage.js
+++ src/routes/login/RetrievePage.js
@@ -5,24 +5,25 @@ import { Form, Icon, Input, Button, Checkbox } from 'antd';
 import titleImg from '../../assets/images/title.png';
 import styles from './RetrievePage.css';
 
-function RetrievePage({ dispatch, form: { getFieldDecorator, validateFields } }) {
-  const [type, setType] = useState('login');
+function RetrievePage({ dispatch, form: { getFieldDecorator, validateFields }, history }) {
+  const [type, setType] = useState('find');
   function handleSubmit(e) {
     e.preventDefault()
     validateFields((err, values) => {
       console.log(err)
       if (!err) {
-        if (type === 'login') {
-          dispatch({
-            type: 'login/checkLogin',
-            payload: values
-          })
+        if (type === 'find') {
+          // dispatch({
+          //   type: 'login/checkLogin',
+          //   payload: values
+          // })
+          setType('reset')
         } else {
           // dispatch({
           //   type: 'login/register',
           //   payload: values
           // })
-          setType('login')
+          history.push('/')
         }
 
       }
@@ -32,79 +33,52 @@ function RetrievePage({ dispatch, form: { getFieldDecorator, validateFields } })
     <div className={styles.loginconter}>
       <img src={titleImg} className={styles.img}></img>
       {
-        type === 'login' ? <div>
-          <h3 className={styles.title}>欢迎！</h3>
+        type === 'find' ? <div>
+          <h3 className={styles.title}>找回密码</h3>
           <div className={styles.normal}>
             <div className={styles.line}></div>
             <Form onSubmit={handleSubmit} className="login-form">
               <Form.Item>
                 {getFieldDecorator('username', {
-                  rules: [{ required: true, message: '请输入账号!' }],
+                  rules: [{ required: true, message: '请输入手机号/邮箱' }],
                 })(
                   <Input
                     prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                    placeholder="请输入账号"
+                    placeholder="请输入手机号/邮箱"
                     size="small"
                   />,
                 )}
               </Form.Item>
               <Form.Item>
                 {getFieldDecorator('password', {
-                  rules: [{ required: true, message: '请输入密码!' }],
+                  rules: [{ required: true, message: '请输入验证码!' }],
                 })(
-                  <Input.Password
-                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                    type="password"
-                    placeholder="请输入密码"
-                  />,
+                  <div className="yzm" style={{ display: 'flex', paddingRight: "2.5rem" }}>
+                    <Input
+                      prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                      type="password"
+                      placeholder="请输入验证码"
+                    />
+                    <Button style={{ background: '#00C8C4', color: '#ffff', border: '1px solid #00C8C4' }} >
+                      发送验证码
+                    </Button>
+                  </div>
                 )}
               </Form.Item>
               <Form.Item>
                 <Button type="primary" htmlType="submit" className={styles.loginformbutton}>
-                  登录
+                  下一步
               </Button>
-              </Form.Item>
-              <Form.Item className={styles.forgetitem}>
-                <div className={styles.forgetcon}>
-                  <span className={styles.forget}>
-                    <Link to='/retrieve'>忘记密码？</Link>
-                  </span>
-                  <span className={styles.zc}>
-                    没有账号？去 <a onClick={() => setType('register')}>注册</a>
-                  </span>
-                </div>
               </Form.Item>
             </Form>
           </div>
 
         </div>
           : <div>
-            <h3 className={styles.title}>欢迎注册！</h3>
+            <h3 className={styles.title}>重置密码</h3>
             <div className={styles.normal}>
               <div className={styles.line}></div>
               <Form onSubmit={handleSubmit} className="login-form">
-                <Form.Item>
-                  {getFieldDecorator('username', {
-                    rules: [{ required: true, message: '请输入账号!' }],
-                  })(
-                    <Input
-                      prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                      placeholder="请输入账号"
-                      size="small"
-                    />,
-                  )}
-                </Form.Item>
-                <Form.Item>
-                  {getFieldDecorator('username', {
-                    rules: [{ required: true, message: '请输入邮箱!' }],
-                  })(
-                    <Input
-                      prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                      placeholder="请输入邮箱"
-                      size="small"
-                    />,
-                  )}
-                </Form.Item>
                 <Form.Item >
                   {getFieldDecorator('password', {
                     rules: [{ required: true, message: '请输入密码!' }],
@@ -128,9 +102,14 @@ function RetrievePage({ dispatch, form: { getFieldDecorator, validateFields } })
                   )}
                 </Form.Item>
                 <Form.Item>
-                  <Button type="primary" htmlType="submit" className={styles.loginformbutton}>
-                    立即注册
-            </Button>
+                  <div style={{ display: "flex", justifyContent: 'space-between' }}>
+                    <Button type="primary" htmlType="submit" style={{ background: "#F45809" }} className={styles.loginformbuttons}>
+                      确认
+                  </Button>
+                    <Button className={styles.loginformbuttons} style={{ marginRight: '2.5rem' }}>
+                      <Link to='/'>取消</Link>
+                    </Button>
+                  </div>
                 </Form.Item>
               </Form>
             </div>
