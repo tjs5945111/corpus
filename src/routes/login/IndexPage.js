fb098d8593d8e76fc9dda7351b599e74c48b160c
童建设 <you@example.com>
2021-04-18 11:46:14 +0800

    feat:注册页面开发

--- src/routes/login/IndexPage.js
+++ src/routes/login/IndexPage.js
@@ -1,72 +1,142 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'dva';
 import styles from './IndexPage.css';
 import titleImg from '../../assets/images/title.png';
 import { Form, Icon, Input, Button, Checkbox } from 'antd';
 
 function IndexPage({ dispatch, form: { getFieldDecorator, validateFields } }) {
-
+  const [type, setType] = useState('login');
   function handleSubmit(e) {
     e.preventDefault()
     validateFields((err, values) => {
       console.log(err)
       if (!err) {
-        dispatch({
-          type: 'login/checkLogin',
-          payload: values
-        })
+        if (type === 'login') {
+          dispatch({
+            type: 'login/checkLogin',
+            payload: values
+          })
+        } else {
+          // dispatch({
+          //   type: 'login/register',
+          //   payload: values
+          // })
+          setType('login')
+        }
+
       }
     })
   }
   return (
     <div className={styles.loginconter}>
       <img src={titleImg} className={styles.img}></img>
-      <div className={styles.normal}>
-        <div className={styles.line}></div>
-        <Form onSubmit={handleSubmit} className="login-form">
-          <Form.Item>
-            {getFieldDecorator('username', {
-              rules: [{ required: true, message: '请输入账号!' }],
-            })(
-              <Input
-                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                placeholder="请输入账号"
-                size="small"
-              />,
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('password', {
-              rules: [{ required: true, message: '请输入密码!' }],
-            })(
-              <Input
-                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                type="password"
-                placeholder="请输入密码"
-              />,
-            )}
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit" className={styles.loginformbutton}>
-              登录
-          </Button>
-          </Form.Item>
-          <Form.Item className={styles.forgetitem}>
-            <div className={styles.forgetcon}>
-              <span className={styles.forget}>
-                忘记密码？
-          </span>
-              <span className={styles.zc}>
-                没有账号？去 <a>注册</a>
+      {
+        type === 'login' ? <div>
+          <h3 className={styles.title}>欢迎登录！</h3>
+          <div className={styles.normal}>
+            <div className={styles.line}></div>
+            <Form onSubmit={handleSubmit} className="login-form">
+              <Form.Item>
+                {getFieldDecorator('username', {
+                  rules: [{ required: true, message: '请输入账号!' }],
+                })(
+                  <Input
+                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                    placeholder="请输入账号"
+                    size="small"
+                  />,
+                )}
+              </Form.Item>
+              <Form.Item>
+                {getFieldDecorator('password', {
+                  rules: [{ required: true, message: '请输入密码!' }],
+                })(
+                  <Input.Password
+                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                    type="password"
+                    placeholder="请输入密码"
+                  />,
+                )}
+              </Form.Item>
+              <Form.Item>
+                <Button type="primary" htmlType="submit" className={styles.loginformbutton}>
+                  登录
+              </Button>
+              </Form.Item>
+              <Form.Item className={styles.forgetitem}>
+                <div className={styles.forgetcon}>
+                  <span className={styles.forget}>
+                    忘记密码？
               </span>
+                  <span className={styles.zc}>
+                    没有账号？去 <a onClick={() => setType('register')}>注册</a>
+                  </span>
+                </div>
+              </Form.Item>
+            </Form>
+          </div>
+
+        </div>
+          : <div>
+            <h3 className={styles.title}>欢迎注册！</h3>
+            <div className={styles.normal}>
+              <div className={styles.line}></div>
+              <Form onSubmit={handleSubmit} className="login-form">
+                <Form.Item>
+                  {getFieldDecorator('username', {
+                    rules: [{ required: true, message: '请输入账号!' }],
+                  })(
+                    <Input
+                      prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                      placeholder="请输入账号"
+                      size="small"
+                    />,
+                  )}
+                </Form.Item>
+                <Form.Item>
+                  {getFieldDecorator('username', {
+                    rules: [{ required: true, message: '请输入邮箱!' }],
+                  })(
+                    <Input
+                      prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                      placeholder="请输入邮箱"
+                      size="small"
+                    />,
+                  )}
+                </Form.Item>
+                <Form.Item >
+                  {getFieldDecorator('password', {
+                    rules: [{ required: true, message: '请输入密码!' }],
+                  })(
+                    <Input.Password
+                      prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                      type="password"
+                      placeholder="请输入密码"
+                    />,
+                  )}
+                </Form.Item>
+                <Form.Item >
+                  {getFieldDecorator('confirm', {
+                    rules: [{ required: true, message: '请确认密码!' }],
+                  })(
+                    <Input.Password
+                      prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                      type="password"
+                      placeholder="请确认密码"
+                    />,
+                  )}
+                </Form.Item>
+                <Form.Item>
+                  <Button type="primary" htmlType="submit" className={styles.loginformbutton}>
+                    立即注册
+            </Button>
+                </Form.Item>
+              </Form>
             </div>
-          </Form.Item>
-        </Form>
-      </div>
+          </div>
+      }
     </div>
-
   );
-
 }
 
 // IndexPage.propTypes = {
